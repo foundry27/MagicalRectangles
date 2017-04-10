93b1950ef5d09141833738b27dbe9391403249bd
Steven Pham <steven.pham.8.2000@gmail.com>
2017-04-10 19:07:42 -0400

    Added name generator to RP namespace

--- name_generator.hpp
+++ name_generator.hpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <functional>
 
-class NameGenerator {
-    std::function<char ()> charProvider;
-    size_t len;
-public:
-    NameGenerator(const std::function<char ()>& charProvider, size_t len);
-    virtual std::string generateName();
-};
+namespace RP {
+    class NameGenerator {
+        std::function<char ()> charProvider;
+        size_t len;
+    public:
+        NameGenerator(const std::function<char ()>& charProvider, size_t len);
+        virtual std::string generateName();
+    };    
+}
 
 #endif
